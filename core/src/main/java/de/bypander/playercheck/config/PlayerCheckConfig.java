f4fcdee29e021fd26ba6d59f7e3e0e75efd9a395
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2024-06-25 16:21:09 +0200

    add missing texture reference

--- core/src/main/java/de/bypander/playercheck/config/PlayerCheckConfig.java
+++ core/src/main/java/de/bypander/playercheck/config/PlayerCheckConfig.java
@@ -4,9 +4,11 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @ConfigName("settings")
+@SpriteTexture("settings.png")
 public class PlayerCheckConfig extends AddonConfig {
 
   @SpriteSlot(x=0, y=0)
