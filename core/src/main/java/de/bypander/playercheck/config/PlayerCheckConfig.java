307a390fb4fe003488262d762f6e62050f37a2a7
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2024-06-25 15:38:49 +0200

    add icon to settings

--- core/src/main/java/de/bypander/playercheck/config/PlayerCheckConfig.java
+++ core/src/main/java/de/bypander/playercheck/config/PlayerCheckConfig.java
@@ -3,11 +3,13 @@ package de.bypander.playercheck.config;
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @ConfigName("settings")
 public class PlayerCheckConfig extends AddonConfig {
 
+  @SpriteSlot(x=0, y=0)
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
